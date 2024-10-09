c87ae9345ae2099304f5dea3222b91851d6adb2b
jhmin99 <mjh012399@gmail.com>
2024-10-09 19:43:32 +0900

    feat: Add RequestDtos

---
+++ src/main/java/com/example/demo/dto/request/diary/DiaryRequestDto.java
@@ -0,0 +1,9 @@
+package com.example.demo.dto.request.diary;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class DiaryRequestDto {
+}
---
+++ src/main/java/com/example/demo/dto/request/user/SignUpRequestDto.java
@@ -0,0 +1,9 @@
+package com.example.demo.dto.request.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class SignUpRequestDto {
+}
