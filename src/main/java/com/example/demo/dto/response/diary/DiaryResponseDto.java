78519c190ee74cbf705f09287abe5763587b8853
ChilledPi <chilledpi7@gmail.com>
2024-11-27 19:54:56 +0900

    feat: Refactor Dtos

--- src/main/java/com/example/demo/dto/response/diary/DiaryResponseDto.java
+++ src/main/java/com/example/demo/dto/response/diary/DiaryResponseDto.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
+import java.time.LocalDate;
 import java.util.List;
 
 
@@ -25,11 +26,25 @@ public class DiaryResponseDto {
     @Schema(description = "List of images associated with the profile")
     private ImageResponseDto profileImage;
 
-    public DiaryResponseDto(Long diaryId, String name, String diaryTitle, Image profileImage) {
+    @Schema(description = "Date of the diary entry", example = "2023-10-12")
+    private LocalDate date;
+
+    @Schema(description = "Number of likes on the diary", example = "100")
+    private Integer likesCount;
+
+    @Schema(description = "List of images associated with the diary")
+    private List<ImageResponseDto> diaryImages;
+
+
+
+    public DiaryResponseDto(Long diaryId, String name, String diaryTitle, Image profileImage, LocalDate date, Integer likesCount, List<Image> diaryImages) {
         this.diaryId = diaryId;
         this.name = name;
         this.diaryTitle = diaryTitle;
         this.profileImage = new ImageResponseDto(profileImage.getId(), profileImage.getUrl());
+        this.date = date;
+        this.likesCount = likesCount;
+        this.diaryImages = diaryImages.stream().map(i -> new ImageResponseDto(i.getId(), i.getUrl())).toList();
     }
 
     @Getter
